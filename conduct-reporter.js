650d94c7284d33136de63943c77e387088da6034
Andrew Siegman <blerrnk@gmail.com>
2018-02-06 20:48:16 -0600

    sendReport function

--- conduct-reporter.js
+++ conduct-reporter.js
@@ -8,23 +8,48 @@ class ConductReporter {
 		}
 	}
 
-	// getMembers performs a GET request at the /members endpoint
-	// and passes the response to a callback function as an object
+	// getMembers performs a GET request to the /members endpoint and
+	// passes the response to the callback as an object
 	getMembers(callback) {
 		var cr = this;
 		var xhr = new XMLHttpRequest();
-		var data = [];
 		xhr.onreadystatechange = function() {
-		    if (xhr.readyState == XMLHttpRequest.DONE) {
-		    	if (xhr.status == 200) {
-		    		callback(JSON.parse(xhr.responseText));
-		    	} else {
-		    		throw new Error("Could not get members: Server at " + cr.server + " did not respond with status code 200");
-		    	}
-		    }
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				if (xhr.status == 200) {
+					if (typeof callback === 'function') {
+						callback(JSON.parse(xhr.responseText));
+					}
+				} else {
+					throw new Error("ConductReport.getMembers: Server at " + cr.server + " did not respond with status code 200");
+				}
+			}
 		}
 		xhr.open('GET', cr.server + "/members", true);
 		xhr.send(null);
+	}
+
+	// sendReport performs a POST to the /report endpoint and passes
+	// true to the callback upon success, or false upon failure
+	sendReport(data, callback) {
+		var cr = this;
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				if (xhr.status != 201 && xhr.status != 400) {
+					throw new Error("ConductReport.sendReport: Server responded with unexpected status code");
+				} else {
+					if (typeof callback === 'function') {
+						if (xhr.status == 201) {
+							callback(true);
+						} else if (xhr.status == 400) {
+							callback(false);
+						}
+					}
+				}
+			}
+		}
+		xhr.open('POST', cr.server + "/report", true);
+		xhr.send(JSON.stringify(data));
 	}
 
 }
